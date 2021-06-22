c613fed5751c78c19389a08a771c1d2bd423c397
Nikitakhandagale <86228743+Nikitakhandagale@users.noreply.github.com>
2021-06-22 12:20:02 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,6 +3,8 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
+    System.out.println(Integer.toBinaryString(n));
+    
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     return "";
   }
